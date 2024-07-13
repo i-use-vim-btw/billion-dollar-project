bfbeeb25ca4fbf548693f9f82ef115e4ccb95cf9
i-use-vim-btw <dsngorkemaslan@gmail.com>
2024-07-13 17:37:50 +0300

    note to emir: you need to refactor your client as we discussed

--- router/client/python/gandi.py
+++ router/client/python/gandi.py
@@ -1,11 +1,14 @@
-import requests, time
+import requests
+import time
+
 
 class GandiClient:
     def __init__(self, uri, api_key="default") -> None:
         self.__URI = uri
         self.header = {'Content-Type': 'application/json', 'Api_key': api_key}
-    
-    def create_collection(self, collection_name = "default", dim=128, id="localhost:19530") -> None:
+
+    def create_collection(self, collection_name="default", dim=128, id="localhost:34779") -> None:
+
         data = {
             "collectionName": collection_name,
             "dimension": dim,
@@ -16,59 +19,55 @@ class GandiClient:
             headers=self.header,
             json=data
         )
-        
+
         res = res.json()
-        
+
         print(res)
-        
+
         if res["code"] == 200:
             print("Collection succesfully created")
         else:
             print("Error in creating collection")
-    
-    
+
     def insert(self, collection_name="default", data=[]):
         in_data = {
             'data': data,
             'collectionName': collection_name,
-            "id": "localhost:19530"
+            "id": "localhost:34779"
         }
-        
+
         res = requests.post(
             'http://' + self.__URI + '/gandi/entities/insert',
             headers=self.header,
             json=in_data
         )
-        
+
         res = res.json()
-        
+
         print(res)
-        
+
         if res["code"] == 200:
             print("Insert successful")
         else:
             print("Insert failed")
-    
-    
-        
+
     def get(self, collection_name="default", ids=[]):
         data = {
             "collectionName": collection_name,
             'id': ids,
-            "host": "localhost:19530"
+            "host": "localhost:34779"
         }
-        
-        
+
         res = requests.post(
             'http://' + self.__URI + '/gandi/entities/get',
             headers=self.header,
             json=data
         )
-        
+
         res = res.json()
-        
+
         print(res)
-        
+
         if res["code"] == 200:
             print(res["data"])
         else:
@@ -78,60 +77,57 @@ class GandiClient:
         up_data = {
             'data': data,
             'collectionName': collection_name,
-            "id": "localhost:19530"
+            "id": "localhost:34779"
         }
-        
+
         res = requests.post(
             'http://' + self.__URI + '/gandi/entities/upsert',
             headers=self.header,
             json=up_data
         )
-        
+
         res = res.json()
-        
+
         print(res)
-        
+
         if res["code"] == 200:
             print("Upsert successful")
         else:
             print("Upsert failed")
-            
+
     def delete(self, collection_name="default", database_name="default", partition_name="default", filter=""):
         delete_data = {
             'collectionName': collection_name,
             'databaseName': database_name,
             'partitionName': partition_name,
             'filter': filter,
-            "id": "localhost:19530"
+            "id": "localhost:34779"
         }
-        
+
         res = requests.post(
             'http://' + self.__URI + '/gandi/entities/delete',
             headers=self.header,
             json=delete_data
         )
-        
+
         res = res.json()
-        
+
         print(res)
-        
+
         if res["code"] == 200:
             print("Delete successful")
         else:
             print("Delete failed")
-        
 
 
-
-
-      
+"""
 client = GandiClient("localhost:8080")
 
 
 collName = "test38"
 
 client.get(collection_name=collName, ids=[4, 5])
-"""
+
 client.create_collection(collection_name=collName, dim=5)
 
 client.insert(collection_name=collName, data=[
