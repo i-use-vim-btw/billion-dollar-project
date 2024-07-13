b747e6152eda90aff73a05d16f74bdb8be875ef2
dsnbey <gorkemaslandsn@gmail.com>
2024-07-13 11:51:49 +0300

    reorganized folder structure

--- py_tests/fetch_tst.py
+++ py_tests/fetch_tst.py
@@ -3,7 +3,7 @@ import json
 
 # 1. Set up a Milvus client
 client = MilvusClient(
-    uri="http://127.0.0.1:40383"
+    uri="http://127.0.0.1:38329"
 )
 
 collection_name = "quick_setup"
---
+++ router/readme.md
@@ -0,0 +1,1 @@
+Contains router/client
